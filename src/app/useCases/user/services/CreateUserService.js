5b02bac3ea905c9c0eafabe90194fe0bc306720c
Devpe007 <anddev007pe@gmail.com>
2022-10-19 18:01:10 -0300

    save the encrypt password in database

--- src/app/useCases/user/services/CreateUserService.js
+++ src/app/useCases/user/services/CreateUserService.js
@@ -1,14 +1,18 @@
+import { hash } from 'bcryptjs';
+
 import db from '../../../../database';
 
 class CreateUserService {
   async execute({
     name, email, phone, password,
   }) {
+    const passwordHash = await hash(password, 8);
+
     const [row] = await db.query(`
       INSERT INTO users(name, email, phone, password)
       VALUES($1, $2, $3, $4)
       RETURNING *
-    `, [name, email, phone, password]);
+    `, [name, email, phone, passwordHash]);
 
     return row;
   };
