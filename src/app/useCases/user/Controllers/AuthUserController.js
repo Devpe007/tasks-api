9b5229145f9e2dc53cfdfc10fc8b22273a71fff8
Devpe007 <anddev007pe@gmail.com>
2022-10-26 08:09:29 -0300

    doing authentication typing

--- src/app/useCases/user/Controllers/AuthUserController.js
+++ src/app/useCases/user/Controllers/AuthUserController.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
+
 import AuthUserService from '../services/AuthUserService';
 
 class AuthUserController {
   async handle(request, response) {
+    const schema = Yup.object().shape({
+      email: Yup.string().required(),
+      password: Yup.string().min(6),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      throw new Error('Fill in the fields correctly');
+    };
+
     const { email, password } = request.body;
 
     const authUserService = new AuthUserService();
