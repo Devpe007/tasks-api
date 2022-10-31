7aa394319a2186f2b721b529f64b6b22fc9d5e6d
Devpe007 <anddev007pe@gmail.com>
2022-10-31 08:24:33 -0300

    showing task category name

--- src/app/useCases/tasks/Services/ShowTaskService.js
+++ src/app/useCases/tasks/Services/ShowTaskService.js
@@ -3,8 +3,9 @@ import db from '../../../../database';
 class ShowTaskService {
   async execute() {
     const rows = await db.query(`
-      SELECT tasks.*
+      SELECT tasks.*, categories.name AS category_name
       FROM tasks
+      LEFT JOIN categories ON categories.id = tasks.category_id
     `);
 
     return rows;
